125ad6f13923a363c8271ec76e10d51d3295fdde
Chris Ant <chrisant@posteo.net>
2023-11-17 15:04:20 +0200

    initial plugin file

---
+++ prism-backup-and-migration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Prism Backup and Migration
+ * Plugin URI: 
+ * Description: Backup and Migration plugin. 
+ * Version: 0.1
+ * Author: Chris Ant
+ * Author URI: 
+ * Text Domain: prism-backup-and-migration
+ * License: GPL2
+ * WC requires at least: 6.4.0
+ * WC tested up to: 8.2
+*/

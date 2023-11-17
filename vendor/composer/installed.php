6d729ea2adff927f5a304570d7848ec4c97da731
Chris Ant <chrisant@posteo.net>
2023-11-17 15:16:42 +0200

    run composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'prism/prism-backup-and-migration',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '10fbb6f949057892900081f2d0f870f17c1a5884',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'prism/prism-backup-and-migration' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '10fbb6f949057892900081f2d0f870f17c1a5884',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
